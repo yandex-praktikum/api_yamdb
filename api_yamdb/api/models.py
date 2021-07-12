a13b9d33040eed728c6282b5d81ee9c3b7e3a504
Gulyaev <egulyaev1@yandex.ru>
2021-07-12 23:48:36 +0300

    Modificate_User_Model (one-to-one)

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=30, blank=True)
+    description = models.TextField(max_length=500, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()

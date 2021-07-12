aad574b05048deab7e23b2f02829e056717c6918
Hash466 <g.h466h@gmail.com>
2021-07-12 17:12:03 +0300

    make urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,10 +4,15 @@ from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt import views
 
-from .views import EmailViewSet
+from .views import EmailViewSet, ReviewViewSet
 
 router = DefaultRouter()
 router.register('auth/email', EmailViewSet, basename='email_post')
+router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews_api',
+)
 
 jwt = [
     path(

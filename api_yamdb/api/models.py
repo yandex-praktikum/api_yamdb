e363b3d5d1787a45d5380081ddbb05744098f4fc
Gulyaev <egulyaev1@yandex.ru>
2021-07-13 12:01:52 +0300

    Modificate_User_Model (AbstractUser)

--- api_yamdb/api/models.py
+++ api_yamdb/api/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('User', 'user'),
+        ('Moderator', 'moderator'),
+        ('Admin', 'admin')
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='User')
+    description = models.TextField(max_length=500, blank=True)

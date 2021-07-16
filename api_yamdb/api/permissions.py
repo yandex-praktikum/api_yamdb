65306112f1a8010119a9d1293c4b2659b1ebe62b
Gulyaev <egulyaev1@yandex.ru>
2021-07-16 18:37:51 +0300

    Add all permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,3 @@
-from rest_framework import status
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
@@ -14,7 +13,12 @@ class IsAdminOrDenied(BasePermission):
     def has_permission(self, request, view):
         if request.user.is_anonymous:
             return False
-        if (request.user.role == "admin" or request.user.is_superuser or view.action == 'me') and (
+        if (request.get_full_path() == '/api/v1/users/me/'
+                and request.method in ('DELETE')):
+            return True
+        if view.action == 'me' and request.method in ('GET', 'PATCH'):
+            return True
+        if (request.user.role == "admin" or request.user.is_superuser) and (
                 request.method in ('GET', 'POST', 'PATCH', 'DELETE')
         ):
             return True
@@ -26,7 +30,8 @@ class IsAdminOrModeratororAuthor(BasePermission):
         if request.method not in ('PATCH', 'DELETE'):
             return True
 
-        return (request.user.role == "admin" or
-                request.user.role == "moderator" or
-                request.user.is_superuser or
-                obj.author == request.user) and (request.method in ('PATCH', 'DELETE',))
+        return ((request.user.role == "admin"
+                or request.user.role == "moderator"
+                or request.user.is_superuser
+                or obj.author == request.user)
+                and (request.method in ('PATCH', 'DELETE',)))

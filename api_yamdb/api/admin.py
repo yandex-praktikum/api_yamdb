ccaf1c294ffb4f35da8746bb6e41de33445644b4
Gulyaev <egulyaev1@yandex.ru>
2021-07-16 22:05:37 +0300

    Final fixes before review

--- api_yamdb/api/admin.py
+++ api_yamdb/api/admin.py
@@ -1,6 +1,45 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Genres, Categories, Titles, Review, Comment
+
+
+class GenresAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "slug")
+    search_fields = ("name",)
+    empty_value_display = "-пусто-"
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "slug")
+    search_fields = ("name",)
+    empty_value_display = "-пусто-"
+
+
+class TitlesAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "year",
+                    "description", "genre", "category")
+    search_fields = ("name",)
+    empty_value_display = "-пусто-"
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("pk", "author", "pub_date",
+                    "text", "title", "score")
+    search_fields = ("author",)
+    empty_value_display = "-пусто-"
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "author", "pub_date",
+                    "review", "text")
+    search_fields = ("author",)
+    empty_value_display = "-пусто-"
+
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Genres, GenresAdmin)
+admin.site.register(Categories, CategoriesAdmin)
+admin.site.register(Titles, TitlesAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -9,7 +9,7 @@ class IsAdminOrReadOnly(BasePermission):
             return request.user.is_superuser
 
 
-class IsAdminOrDenied(BasePermission):
+class IsAdminOrMeEndpointOrDenied(BasePermission):
     def has_permission(self, request, view):
         if request.user.is_anonymous:
             return False
@@ -18,7 +18,7 @@ class IsAdminOrDenied(BasePermission):
             return True
         if view.action == 'me' and request.method in ('GET', 'PATCH'):
             return True
-        if (request.user.role == "admin" or request.user.is_superuser) and (
+        if (request.user.role == 'admin' or request.user.is_superuser) and (
                 request.method in ('GET', 'POST', 'PATCH', 'DELETE')
         ):
             return True
@@ -30,8 +30,8 @@ class IsAdminOrModeratororAuthor(BasePermission):
         if request.method not in ('PATCH', 'DELETE'):
             return True
 
-        return ((request.user.role == "admin"
-                or request.user.role == "moderator"
-                or request.user.is_superuser
-                or obj.author == request.user)
+        return ((request.user.role == 'admin'
+                 or request.user.role == 'moderator'
+                 or request.user.is_superuser
+                 or obj.author == request.user)
                 and (request.method in ('PATCH', 'DELETE',)))

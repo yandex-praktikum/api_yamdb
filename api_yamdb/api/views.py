79368b9e530343967236fe93eb242a4c62ae4237
ezelenenkaya1580 <80488007+ezelenenkaya1580@users.noreply.github.com>
2021-07-16 11:40:30 +0300

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -83,7 +83,7 @@ class TitlesViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAdminOrReadOnly,)
 
     def get_serializer_class(self):
-        if self.action == 'list' or 'retrieve':
+        if self.action == 'list' or self.action == 'retrieve':
             return TitlesGetSerializer 
         return TitlesPostSerializer
 

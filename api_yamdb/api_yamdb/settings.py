02ae3ec85542b623bda2f7b8da67df941c4157e5
me <me@MacBook-Pro-me.local>
2022-05-17 01:56:18 +0300

    added jwt

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -113,7 +113,9 @@ AUTH_USER_MODEL = 'users.User'
 
 
 REST_FRAMEWORK = {
-
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [

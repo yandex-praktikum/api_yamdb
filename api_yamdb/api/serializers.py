69682cb0e2ed48c69402a02d1488bc788a0f35f0
Василий Ямутин <yamutin2010@yandex.ru>
2023-01-12 12:38:56 +0300

    Добавлены сериализаторы ReviewSerializer, CommentSerializer

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from reviews.models import Review, Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'author', 'score', 'pub_date')
+
+    def validate(self, data):
+        request = self.context.get('request')
+        if request.method == 'POST':
+            author = request.user
+            title_id = self.context.get('view').kwargs.get('title_id')
+            if Review.objects.filter(author=author, title=title_id).exists():
+                raise serializers.ValidationError(
+                    'Вы уже оставляли отзыв на это произведение'
+                )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment

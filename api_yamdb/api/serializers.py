5ff32980c295f8d194039e1d9742a93f44b92337
Василий Ямутин <yamutin2010@yandex.ru>
2023-01-12 12:53:13 +0300

    Добавлены вьюсеты ReviewViewSet, CommentViewSet

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Review, Comment
+from reviews.models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 
-# Create your views here.
+from .permissions import ReadOnlyOrAuthorOrAdminOrModerator
+from .serializers import ReviewSerializer, CommentSerializer
+from reviews.models import Title, Review
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [ReadOnlyOrAuthorOrAdminOrModerator]
+
+    def get_title(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        return title
+
+    def get_queryset(self):
+        return self.get_title().reviews.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            title=self.get_title()
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [ReadOnlyOrAuthorOrAdminOrModerator]
+
+    def get_review(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(Review, id=review_id)
+        return review
+
+    def get_queryset(self):
+        return self.get_review().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            review=self.get_review()
+        )

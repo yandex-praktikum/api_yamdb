6fed1f178f9a2b7004bac12a64cdbafae75c5c02
Василий Ямутин <yamutin2010@yandex.ru>
2023-01-15 16:57:49 +0300

    Создание директории 'v1' и перевод соотвствующих файлов в неё

---
+++ api_yamdb/api/v1/urls.py
@@ -0,0 +1,38 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import (
+    UsersViewSet,
+    TitleViewSet,
+    CategoryViewSet,
+    GenreViewSet,
+    CommentViewSet,
+    ReviewViewSet,
+    SignUpView,
+    GetToken,
+)
+
+
+v1_router = routers.DefaultRouter()
+v1_router.register('users', UsersViewSet, basename='users')
+v1_router.register(r'categories', CategoryViewSet, basename='categories')
+v1_router.register(r'genres', GenreViewSet, basename='genres')
+v1_router.register(r'titles', TitleViewSet, basename='titles')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='reviews'
+)
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+
+auth_urls = [
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('token/', GetToken.as_view(), name='token'),
+]
+
+urlpatterns = [
+    path('', include(v1_router.urls)),
+    path('auth/', include(auth_urls)),
+]

052c63ea660621d900d37539cf6e5df867a72dab
iwanttobe00 <xxhh0@mail.ru>
2023-01-30 20:52:46 +0400

    reset

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -146,15 +146,18 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAdminModeratorOwnerOrReadOnly]
+    permission_classes = [IsAdminModeratorOwnerOrReadOnly, ]
 
     def get_queryset(self):
-        review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
         return review.comments.all()
 
     def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
-        review_id = self.kwargs.get('review_id')
-        review = get_object_or_404(Review, id=review_id, title=title_id)
+        review = get_object_or_404(
+            Review,
+            id=self.kwargs.get('review_id'))
         serializer.save(author=self.request.user, review=review)
+
 

a35303508785a7ef57dc0f40050628f7a9e0cc6e
Роман Петушков <stvoid333@gmail.com>
2023-02-16 01:35:59 +0300

    Переписанная система аутентификаций,пермишены,токены теперь просто рандомное число

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import get_token, register, UserViewSet
+from .views import GetTokenView, RegistrationView, UserViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/auth/signup/', register, name='register'),
-    path('v1/auth/token/', get_token, name='token')
+    path('v1/auth/signup/', RegistrationView.as_view(), name='register'),
+    path('v1/auth/token/', GetTokenView.as_view(), name='token')
 ]

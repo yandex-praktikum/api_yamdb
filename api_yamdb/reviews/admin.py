7ec2c41c953bc7504e6d57dd6ee4fe4f4eb0662e
Амир Багаутдинов <abagautdinoff@yandex.ru>
2023-02-16 10:26:59 +0400

    Версия 2

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from .models import Review, Comment, Rating
+from .models import Review, Comment
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    """Настройка админки отзывов к произведеням."""
+    list_display = ('id', 'author', 'title', 'text', 'score', 'pub_date')
+    search_fields = ('text',)
+    list_filter = ('score', 'author', 'pub_date')
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    """Настройка админки комментариев к отзывам."""
+    list_display = ('id', 'author', 'review', 'text', 'pub_date')
+    search_fields = ('text',)
+    list_filter = ('author', 'pub_date')
+    empty_value_display = '-пусто-'
 
 
 admin.site.register(Review)
 admin.site.register(Comment)
-admin.site.register(Rating)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,57 +1,67 @@
 from django.db import models
-from .users import User
-from .titles import Title
-
-RATE_CHOICES = [(i) for i in range(11)]
-
-
-class Rating(models.Model):
-    rating = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE)
-    title = models.ForeignKey(
-        Title, on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ('author', 'title')
-        verbose_name = 'Оценка'
-        ordering = ['rating']
-
-    def average_rating(self):
-        all_ratings = list(map(lambda x: x.value, self.reviews.all()))
-        sum_all_ratings = sum(all_ratings)
-        return (round(sum_all_ratings / len(all_ratings), 0))
-
-    def __str__(self):
-        return self.rating
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Review(models.Model):
+    """Модель отзывов к произведениям."""
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='reviews')
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
     title = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='reviews')
+        Title,
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
     text = models.TextField()
-    score = models.ForeignKey(
-        Rating, default=0, null=True, blank=True)
+    score = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)],
+        verbose_name='Рейтинг произведения'
+    )
     pub_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        verbose_name='Дата добавления',
+        auto_now_add=True
+    )
     slug = models.SlugField(unique=True)
 
     class Meta:
-        unique_together = ('author', 'title')
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'],
+                name='Одно произведение- один отзыв одному автору'
+            )
+        ]
         verbose_name = 'Отзыв'
-        ordering = ['reviews']
+        verbose_name_plural = 'Отзывы'
+        ordering = ('-pub_date',)
 
     def __str__(self):
-        return self.title
+        return self.text
 
 
 class Comment(models.Model):
+    """Модель комментариев к отзывам."""
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     review = models.ForeignKey(
-        Review, on_delete=models.CASCADE, related_name='comments')
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     text = models.TextField()
     pub_date = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        verbose_name='Дата добавления',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text

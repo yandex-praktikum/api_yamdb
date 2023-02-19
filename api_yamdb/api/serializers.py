079f04dc53d35996287ac2eef643056316f8a8a1
legion51 <57018749+legion51@users.noreply.github.com>
2023-02-19 17:00:09 +0300

    comment_review

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,9 +3,96 @@ from rest_framework import serializers
 from django.core.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 
-from reviews.models import Comment, Review, Title
+from reviews.models import User, Genre, Category, Title, TitleGenre, Review, Comment
 
-from .serializers import CommentSerializer, ReviewSerializer
+
+class TokenSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    confirmation_code = serializers.CharField()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ("email", "username")
+
+    def validate_username(self, data):
+        if data == "me":
+            raise serializers.ValidationError("me не может быть использовано "
+                                              "в качестве имени пользователя")
+        return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ("email", "username", "first_name", "last_name", "bio",
+                  "role")
+
+    def validate_username(self, data):
+        if data == "me":
+            raise serializers.ValidationError("me не может быть использовано "
+                                              "в качестве имени пользователя")
+        return data
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name', 'slug')
+        model = Genre
+
+
+class TitleGenreSerializer(serializers.ModelSerializer):
+    # slug = serializers.SlugRelatedField(slug_field='slug')
+    class Meta:
+        fields = ('slug')
+        model = Genre
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('name', 'slug')
+        model = Category
+
+
+class TitleCategorySerializer(CategorySerializer):
+    def to_internal_value(self, data):
+        slug = data
+        return {
+            'slug': slug,
+        }
+
+
+class TitleReadSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        model = Title
+
+
+class TitleWriteSerializer(serializers.ModelSerializer):
+    category = TitleCategorySerializer()
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(), many=True, slug_field="slug"
+    )
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        model = Title
+
+    def create(self, validated_data):
+        genres = validated_data.pop('genre')
+        category_slug = validated_data.pop('category').get('slug')
+        category = get_object_or_404(Category, slug=category_slug)
+        title = Title.objects.create(category=category, **validated_data)
+        title.save()
+        for each in genres:
+            genre = Genre.objects.get(name=each)
+            TitleGenre.objects.get_or_create(title=title, genre=genre)
+        return title
 
 
 class ReviewSerializer(serializers.ModelSerializer):

888319544954f078829a9b879734321fa7ccdaf7
Роман Петушков <stvoid333@gmail.com>
2023-02-20 19:49:43 +0300

    тесты проходят точно

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -52,9 +52,13 @@ class CategorySerializer(serializers.ModelSerializer):
 class TitleReadSerializer(serializers.ModelSerializer):
     genre = GenreSerializer(many=True)
     category = CategorySerializer()
+    rating = serializers.IntegerField(
+        source='reviews__score__avg',
+        read_only=True
+    )
 
     class Meta:
-        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        fields = '__all__'
         model = Title
 
 
@@ -66,9 +70,13 @@ class TitleWriteSerializer(serializers.ModelSerializer):
     genre = serializers.SlugRelatedField(
         queryset=Genre.objects.all(), many=True, slug_field="slug"
     )
+    rating = serializers.IntegerField(
+        source='reviews__score__avg',
+        read_only=True
+    )
 
     class Meta:
-        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        fields = '__all__'
         model = Title
 
 

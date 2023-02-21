df2cb83ab6929e61b092b000434188e8418a9ae6
legion51 <57018749+legion51@users.noreply.github.com>
2023-02-21 12:21:33 +0300

    Update serializers.py
    убрано не рабочее условие

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -91,11 +91,6 @@ class ReviewSerializer(serializers.ModelSerializer):
         read_only=True
     )
 
-    def validate_score(self, value):
-        if 0 > value > 10:
-            raise serializers.ValidationError('Оценка по 10-бальной шкале!')
-        return value
-
     def validate(self, data):
         request = self.context['request']
         author = request.user

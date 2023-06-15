199096600213c79accb48c92802f015df51d751a
ГорловаЕА <gorlikvolg@yandex.ru>
2023-06-16 00:47:50 +0300

    Started viewset

---
+++ api/views.py
@@ -0,0 +1,44 @@
+from rest_framework import viewsets, permissions
+from django.shortcuts import get_object_or_404
+from reviews.models import Comments, Reviews, Title
+from serializers import CommentSerializer, ReviewSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Reviews.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(
+            Title,
+            pk=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(
+            Reviews,
+            pk=title_id)
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comments.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(
+            Reviews,
+            pk=review_id)
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get('review_id')
+        review = get_object_or_404(
+            Reviews,
+            pk=review_id)
+        serializer.save(author=self.request.user, review=review)

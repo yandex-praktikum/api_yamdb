8931098646a0f1dfce668242ea5ef797d4249f6b
Чащихин Сергей <Inko-75@yandex.ru>
2023-06-21 00:09:30 +0300

    update+ permissions

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -25,9 +25,10 @@ class IsStaffOrReadOnly(BasePermission):
                 or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
-        return (request.user.is_superuser
-                or request.user.is_admin
-                or request.user.is_moderator)
+        return (request.user.is_authenticated
+                and (request.user.is_superuser
+                     or request.user.is_admin
+                     or request.user.is_moderator))
 
 
 class IsAdminOrReadOnly(BasePermission):

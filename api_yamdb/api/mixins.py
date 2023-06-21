740efe1f50ce5a6b1449d8b3c9b61a81cd8bd0dd
Чащихин Сергей <Inko-75@yandex.ru>
2023-06-22 01:50:10 +0300

    промежуточные изменения

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,13 +1,13 @@
 from rest_framework import mixins, viewsets, filters
 
-from users.permissions import IsAdminOrReadOnly
+from users.permissions import IsAdminOnly
 
 
 class DestroyCreateListMixins(mixins.CreateModelMixin,
                               mixins.ListModelMixin,
                               mixins.DestroyModelMixin,
                               viewsets.GenericViewSet):
-    permission_classes = (IsAdminOrReadOnly,)
+    permission_classes = (IsAdminOnly,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'

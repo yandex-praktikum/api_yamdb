35c1eabd7fae1fdf35b54ec6fe635cd01bc41a99
Павел Кошкаров <pavel.koshkarov3@gmail.com>
2023-07-04 15:02:52 +0400

    Настроил отправку почты

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -106,6 +107,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend' 
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [

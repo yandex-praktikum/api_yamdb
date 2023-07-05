13a5d45a1f5dc77c9ad89971d2c57929f7374957
Павел Кошкаров <pavel.koshkarov3@gmail.com>
2023-07-05 19:24:02 +0400

    Внёс изменения

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,21 +1,25 @@
 from rest_framework import viewsets
 from reviews.models import Category, Genre, Title
 from .mixins import ListCreateDestroyViewSet
+from .serializers import (
+    CategoriesSerializer, GenresSerializer,
+    TitlesPostSerializer, ReviewSerializer,
+    CommentSerializer)
 
 
 class CategoryViewSet(ListCreateDestroyViewSet):
     queryset = Category.objects.all()
-    serializer_class = CategorySerializer
+    serializer_class = CategoriesSerializer
 
 
 class GenreViewSet(ListCreateDestroyViewSet):
     queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
+    serializer_class = GenresSerializer
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
-    serializer_class = TitleSerializer
+    serializer_class = TitlesPostSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):

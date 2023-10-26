00dd2b2e0d8de233072d221aa02274d190ffee9c
РР›СЋРґРјРёР»Р° РќРёР¶РµРіРѕСЂРѕРґРѕРІР° <lyudmilanizhegorodova@yandex.ru>
2023-10-26 21:30:04 +0300

    Add validators and constraints

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,8 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [

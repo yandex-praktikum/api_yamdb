f9a43faf7f8292d6839d4e765ea286348da0231f
Никита Кузнецов <eudaimonia22@yandex.ru>
2024-04-24 00:18:29 +0700

    made models: Title, Genre, Category

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50,
+                            unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50,
+                            unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField(blank=True,
+                                   null=True)
+    genres = models.ManyToManyField(
+        Genre, through='TitleGenre'
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True,
+        related_name='titles'
+    )
+
+
+class TitleGenre(models.Model):
+    title = models.ForeignKey(Title, on_delete=models.SET_NULL,
+                              related_name='titles'
+    )
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL,
+                              related_name='genres')

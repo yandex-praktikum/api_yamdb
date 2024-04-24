91674993ef18652ca302adda7104901030f74887
Raif Shajdullin <fiar4ik97@gmail.com>
2024-04-24 17:51:30 +0300

    add custom user model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+USER_ROLES = (
+    ('user', 'пользователь'),
+    ('moderator', 'модератор'),
+    ('admin', 'администратор'),
+)
+
+
+class YamdbUser(AbstractUser):
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField(
+        'Роль пользователя',
+        max_length=16,
+        choices=USER_ROLES,
+        default='user'
+    )
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+        ordering = ['pk']
+
+    def __str__(self):
+        return self.username

1cf811047b2a30f512726d590ae819c1df8f0d77
Никита Кузнецов <eudaimonia22@yandex.ru>
2024-04-25 20:07:39 +0700

    made urls, viewsets and serializers

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,57 @@
+import datetime as dt
+
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueValidator
+from reviews.models import Category, Genre, Title
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+        validators=[UniqueValidator(queryset=Title.objects.all())]
+    )
+
+    genre = SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True,
+        validators=[UniqueValidator(queryset=Title.objects.all())]
+    )
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'description',
+                  'genre', 'category')  # rating
+        read_only_fields = ('id',)  # rating
+
+    def validate_year(self, value):
+        current_year = dt.datetime.today().year
+        if not (value <= current_year):  # возможно: if value
+            raise serializers.ValidationError('Проверьте год выпуска!')
+        return value
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(
+        max_length=50,
+        validators=[UniqueValidator(queryset=Category.objects.all())]
+    )
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug',)
+        lookup_field = 'slug'
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(
+        max_length=50,
+        validators=[UniqueValidator(queryset=Genre.objects.all())]
+    )
+
+    class Meta:
+        model = Genre
+        fields = ('name', 'slug',)
+        lookup_field = 'slug'
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets  # , filters, permissions
+from reviews.models import Category, Genre, Title
 
-# Create your views here.
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    http_method_names = ['get', 'post',
+                         'patch', 'delete']
+
+
+class ListCreateDestroyViewSet(mixins.ListModelMixin,
+                               mixins.CreateModelMixin,
+                               mixins.DestroyModelMixin,
+                               viewsets.GenericViewSet):
+    pass
+
+
+class CategoryViewSet(ListCreateDestroyViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+
+
+class GenreViewSet(ListCreateDestroyViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    lookup_field = 'slug'

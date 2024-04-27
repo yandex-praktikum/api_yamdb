c048a4d6237fda2a74cd44e3b137551e15e6c7fe
Raif Shajdullin <fiar4ik97@gmail.com>
2024-04-27 21:10:27 +0300

    add jwt tokens

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView
 from users.views import (UserRegistrationView, UserRetrieveUpdateAPIView,
                          UserViewSet)
 
@@ -12,5 +13,7 @@ urlpatterns = [
     path('v1/users/me/', UserRetrieveUpdateAPIView.as_view(), name='user_self'),
     path('v1/auth/signup/', UserRegistrationView.as_view(),
          name='user_registration'),
+    path('v1/auth/token/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('v1/', include(router_v1.urls)),
 ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,6 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -107,3 +110,15 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 AUTH_USER_MODEL = 'users.YamdbUser'
 
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}

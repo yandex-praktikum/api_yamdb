11a7d967bbdb0555f3ad68c6c1705a7e2d436a21
Raif Shajdullin <fiar4ik97@gmail.com>
2024-04-27 22:44:49 +0300

    add permissions for admin, moderator, user, author

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,77 @@
+from rest_framework import permissions
+
+
+class AuthorOrReadOnly(permissions.BasePermission):
+    """Разрешения на редактирование и удаление для автора."""
+
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
+
+
+class UserSelfAccess(permissions.BasePermission):
+    """Разрешения для владельца профиля.
+
+    Просмотр и изменение данных профиля."""
+
+    def has_object_permission(self, request, view, obj):
+        return obj.username == request.user.username
+
+
+# class AdminOrReadOnly(permissions.BasePermission):
+
+#     def has_permission(self, request, view):
+#         return (
+#             request.method in permissions.SAFE_METHODS
+#             or (request.user.role == 'admin')
+#             or request.user.is_superuser
+#         )
+
+#     def has_object_permission(self, request, view, obj):
+#         return (
+#             request.method in permissions.SAFE_METHODS
+#             or (request.user.role == 'admin')
+#             or request.user.is_superuser
+#         )
+
+
+class AdminAccess(permissions.BasePermission):
+    """Разрешения для администратора и суперпользователя."""
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return (
+                (request.user.role == 'admin')
+                or request.user.is_superuser
+            )
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            return (
+                (request.user.role == 'admin')
+                or request.user.is_superuser
+            )
+        return False
+
+
+class ModeratorAccess(permissions.BasePermission):
+    """Разрешения для модератора и суперпользователя."""
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return (
+                (request.user.role == 'moderator')
+                or request.user.is_superuser
+            )
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated:
+            return (
+                (request.user.role == 'moderator')
+                or request.user.is_superuser
+            )
+        return False

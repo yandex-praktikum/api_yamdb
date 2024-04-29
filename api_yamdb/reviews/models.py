b4efef65e9aa1b0ebd542516ef2b743d3df53e26
Дмитрий <>
2024-04-29 17:48:19 +0300

    add models comments and reviwes

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from users.models import YamdbUser
 
 
 # Можно добавить BaseModel в соседний файл
@@ -53,7 +56,7 @@ class Title(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True,
         related_name='titles',
-        #related_name='category',
+        # related_name='category',
         verbose_name='Категория произведения'
     )
 
@@ -77,3 +80,72 @@ class TitleGenre(models.Model):
 
     def __str__(self):
         return f'{self.title} {self.genre}'
+
+
+# Надо не забыть создать миграции для отзывов
+# Сейчас пока не работают миграции
+class Review(models.Model):
+    text = models.TextField(verbose_name='текст отзыва')
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        verbose_name='Название произведения'
+    )
+
+    author = models.ForeignKey(
+        YamdbUser,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        related_name='reviews')
+    score = models.PositiveSmallIntegerField(
+        default=10,
+        validators=[
+            MinValueValidator(
+                1, 'Значение рейтинга должно быть больше 1.'),
+            MaxValueValidator(
+                10, 'Значение рейтинга должно быть меньше 10.')],
+        verbose_name='Рейтинг')
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата публикации отзыва'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['author', 'title'],
+                                    name='unique_author_title')
+        ]
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        # в модель YamdbUser нужно добавить поле username
+        return f'Отзыв {self.author.username} на {self.title.name}'
+
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name='text')
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        verbose_name='Комментарии',
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        YamdbUser,
+        on_delete=models.CASCADE,
+        verbose_name='Автор комментария',
+        related_name='comments',
+        null=True
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации комментария',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        ordering = ('-pub_date',)
+
+    def __str__(self):
+        return self.text

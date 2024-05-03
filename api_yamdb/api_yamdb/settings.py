d10cc9493b1a963602b58844441f3e6d0597c226
Raif Shajdullin <fiar4ik97@gmail.com>
2024-05-03 11:16:37 +0300

    resolve settings R_F conflict

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -116,6 +116,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],

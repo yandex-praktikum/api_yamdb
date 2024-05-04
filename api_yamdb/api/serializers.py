247f6539afed22052dd012d167370c9aded22f16
NikitaPreis <148185205+NikitaPreis@users.noreply.github.com>
2024-05-04 20:29:12 +0700

    Update serializers.py (back)

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,17 +1,19 @@
 import datetime as dt
 
-from django.shortcuts import get_object_or_404
+from django.db.models import Avg, Count, Max
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueValidator
-from reviews.models import Category, Genre, Title, Review, Comment
-from django.forms import ValidationError
+from reviews.models import Category, Genre, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
     slug = serializers.SlugField(
         max_length=50,
         validators=[UniqueValidator(queryset=Category.objects.all())]
+    )
+    name = serializers.CharField(
+        max_length=256
     )
 
     class Meta:
@@ -25,9 +27,84 @@ class GenreSerializer(serializers.ModelSerializer):
         max_length=50,
         validators=[UniqueValidator(queryset=Genre.objects.all())]
     )
-    # name = serializers.CharField(max_length=256,)  # Уточнить
+    name = serializers.CharField(
+        max_length=256
+    )
 
     class Meta:
         model = Genre
         fields = ('name', 'slug',)
         lookup_field = 'slug'
+
+
+class TitleCreateUpdateSerializer(serializers.ModelSerializer):
+    genre = SlugRelatedField(
+        many=True,
+        slug_field='slug',
+        queryset=Genre.objects.all()
+    )
+    category = SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+    name = serializers.CharField(
+        max_length=256
+    )
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'description',
+            'genre', 'category', # rating
+        )
+        read_only_fields = ('id',)
+
+    def validate_year(self, value):
+        current_year = dt.datetime.today().year
+        if not (value <= current_year):
+            raise serializers.ValidationError('Проверьте год выпуска!')
+        return value
+
+    def update(self, instance, validated_data):
+        instance.category = validated_data.get('category')
+        instance.genre.set(validated_data.get('genre'))
+        instance.save()
+        return instance
+
+# Рабочий без rating
+class TitleReadSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True, read_only=True)
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = Title
+        fields = (
+            'id', 'name', 'year', 'description',
+            'genre', 'category'
+        )
+
+# Тесты + Шаблоны для rating
+# class TitleReadSerializer(serializers.ModelSerializer):
+#     genre = GenreSerializer(many=True, read_only=True)
+#     category = CategorySerializer(read_only=True)
+#     rating = serializers.SerializerMethodField()
+
+#     class Meta:
+#         model = Title
+#         fields = (
+#             'id', 'name', 'year', 'rating', 'description',
+#             'genre', 'category'
+        # )
+
+    # def get_rating(self, obj):
+    #     Возможно добавить .select_related('reviews')
+    #     rating = Title.objects.filter(
+    #         title_id=obj.id
+    #         ).annotate(Avg('reviews__score', default=0))  # Возможно aggrregate
+    #     print(rating)
+    #     return round(rating['rating__avg'])
+
+    # def get_rating(self, obj):
+    #     rating = Title.objects.filter(
+    #         id=obj.id).aggregate(Avg('year'))
+    #     return round(rating['year__avg'])

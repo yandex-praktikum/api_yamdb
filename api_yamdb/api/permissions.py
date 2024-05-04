2b4bd033e4a44d44c081453562933141df6a922c
Дмитрий <>
2024-05-04 20:03:04 +0300

    add views comment and review

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -49,3 +49,19 @@ class ModeratorAccess(permissions.BasePermission):
         return (request.user.is_authenticated
                 and (request.user.role == 'moderator'
                      or request.user.is_superuser))
+
+
+class AuthorOrModeratorOrAdminAccess(permissions.BasePermission):
+    """Разрешения для модератора и суперпользователя."""
+
+    def has_permission(self, request, view, obj):
+        return (request.user.is_authenticated
+                and (request.user.role == 'moderator'
+                     or request.user.is_superuser
+                     or request.user == obj.author))
+
+    def has_object_permission(self, request, view, obj):
+        return (request.user.is_authenticated
+                and (request.user.role == 'moderator'
+                     or request.user.is_superuser
+                     or request.user == obj.author))

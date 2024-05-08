c13c5a138dc6b192c8022275893acc7ffe6b1ee3
Raif Shajdullin <fiar4ik97@gmail.com>
2024-05-08 16:20:17 +0300

    change users endpoint, delete users/me

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,16 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from users.views import (TokenObtainView, UserSelfRetrieveUpdateAPIView,
-                         UserSignupView, UserViewSet)
 
-from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                    ReviewViewSet, TitleViewSet)
+from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                       ReviewViewSet, TitleViewSet)
+from users.views import TokenObtainView, UserSignupView, UserViewSet
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 
-router_v1.register(r'users', UserViewSet, basename='user')
+router_v1.register('users', UserViewSet, basename='user')
 router_v1.register(r'titles', TitleViewSet, basename='title')
 router_v1.register(r'categories', CategoryViewSet, basename='category')
 router_v1.register(r'genres', GenreViewSet, basename='genre')
@@ -21,11 +20,6 @@ router_v1.register(
     CommentViewSet, basename='comment')
 
 urlpatterns = [
-    path(
-        'v1/users/me/',
-        UserSelfRetrieveUpdateAPIView.as_view(),
-        name='user_self'
-    ),
     path(
         'v1/auth/signup/',
         UserSignupView.as_view(),

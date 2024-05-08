39c6d7352d6d7ec5bb70472085f046ad98d94f7a
Raif Shajdullin <fiar4ik97@gmail.com>
2024-05-08 16:24:56 +0300

    change admin for user

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from users.models import YamdbUser
 
-UserAdmin.fieldsets += (
-    ('Extra Fields', {'fields': ('bio', 'role', 'confirmation_code')}),
-)
+User = get_user_model()
 
-admin.site.register(YamdbUser, UserAdmin)
+
+@admin.register(User)
+class YamdbUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets
+    fieldsets += (('Extra Fields', {'fields': ('bio', 'role')}),)
+    list_display = ('pk', 'username', 'email', 'role')
+    list_editable = ('role',)
+    list_filter = ('role',)

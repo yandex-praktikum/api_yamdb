071516c6dce1189197b7d44738fe87c90f8a8c89
Raif Shajdullin <fiar4ik97@gmail.com>
2024-05-11 10:26:21 +0300

    change admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
+from core import constants as const
+from reviews.models import Category, Genre, Title, Review, Comment, TitleGenre
+
 User = get_user_model()
+
+
+class TitleGenreInline(admin.TabularInline):
+    model = TitleGenre
+    extra = 1
 
 
 @admin.register(User)
@@ -12,3 +20,49 @@ class YamdbUserAdmin(UserAdmin):
     list_display = ('pk', 'username', 'email', 'role')
     list_editable = ('role',)
     list_filter = ('role',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    list_editable = ('name', 'slug')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    list_editable = ('name', 'slug')
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    inlines = [TitleGenreInline]
+    list_display = ('pk', 'name', 'year', 'category', 'get_genre_list')
+    list_editable = ('category',)
+    list_select_related = ['category',]
+    search_fields = ('name',)
+    list_per_page = const.OBJECTS_PER_PAGE
+
+    def get_genre_list(self, obj):
+        return ", ".join([genre.name for genre in obj.genre.all()])
+    get_genre_list.short_description = 'Жанры'
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'author', 'score', 'text', 'pub_date')
+    list_select_related = ['title', 'author']
+    search_fields = ('title',)
+    list_filter = ('pub_date',)
+    ordering = ['-pub_date']
+    list_per_page = const.OBJECTS_PER_PAGE
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'review', 'author', 'text', 'pub_date')
+    list_select_related = ['review', 'author']
+    search_fields = ('review',)
+    list_filter = ('pub_date',)
+    ordering = ['-pub_date']
+    list_per_page = const.OBJECTS_PER_PAGE

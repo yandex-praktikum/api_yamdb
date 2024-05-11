5a264f781da93603c99251b84d8b79c7cd8746ad
Никита Кузнецов <eudaimonia22@yandex.ru>
2024-05-11 14:23:04 +0700

    updated readme, serializers, views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -10,6 +10,7 @@ from api.permissions import (AdminOrReadOnlyAccess,
 from api.serializers import (CategorySerializer, CommentSerializers,
                              GenreSerializer, ReviewSerializer,
                              TitleCreateUpdateSerializer, TitleReadSerializer)
+from core.views import get_title_object, get_review_object
 from reviews.models import Category, Genre, Review, Title
 
 
@@ -58,16 +59,13 @@ class ReviewViewSet(viewsets.ModelViewSet):
     permission_classes = (AuthorOrModeratorOrAdminAccess,)
     http_method_names = ['get', 'post',
                          'patch', 'delete']
-    lookup_field = 'id'
 
     def get_queryset(self):
-        title_id = self.kwargs.get('title_id')
-        new_queryset = Review.objects.filter(title_id=title_id)
-        return new_queryset
+        title = get_title_object(self)
+        return title.reviews.all()
 
     def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
-        title = get_object_or_404(Title.objects, id=title_id)
+        title = get_title_object(self)
         serializer.save(author=self.request.user, title=title)
 
 
@@ -76,20 +74,11 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (AuthorOrModeratorOrAdminAccess,)
     http_method_names = ['get', 'post',
                          'patch', 'delete']
-    lookup_field = 'id'
 
     def get_queryset(self):
-        review = get_object_or_404(
-            Review,
-            pk=self.kwargs.get('review_id'),
-            title_id=self.kwargs.get('title_id')
-        )
+        review = get_review_object(self)
         return review.comments.all()
 
     def perform_create(self, serializer):
-        review = get_object_or_404(
-            Review,
-            pk=self.kwargs.get('review_id'),
-            title_id=self.kwargs.get('title_id')
-        )
+        review = get_review_object(self)
         serializer.save(author=self.request.user, review=review)

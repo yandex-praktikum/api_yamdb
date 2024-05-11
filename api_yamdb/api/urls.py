214f9d6ffb2c22bfebb142fbdc0a4591e603d8f9
Raif Shajdullin <fiar4ik97@gmail.com>
2024-05-11 10:56:28 +0300

    fix urlpatterns

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -19,16 +19,24 @@ router_v1.register(
     r'titles/(?P<title_id>[^/.]+)/reviews/(?P<review_id>[^/.]+)/comments',
     CommentViewSet, basename='comment')
 
-urlpatterns = [
+v1_auth_urls = [
     path(
-        'v1/auth/signup/',
+        'signup/',
         UserSignupView.as_view(),
         name='user_signup'
     ),
     path(
-        'v1/auth/token/',
+        'token/',
         TokenObtainView.as_view(),
         name='token_obtain'
     ),
-    path('v1/', include(router_v1.urls)),
+]
+
+v1_urls = [
+    path('', include(router_v1.urls)),
+    path('auth/', include(v1_auth_urls)),
+]
+
+urlpatterns = [
+    path('v1/', include(v1_urls)),
 ]
